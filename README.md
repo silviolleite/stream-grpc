67f1ceb3efac7e998e83d6c4c465e93ee35aabb0
Silvio Leite <silviolleite@gmail.com>
2020-02-21 22:27:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Example of use gRPC with golang
+===================================================
+
+The server and client demonstrate how to use grpc go libraries.
+
+See the definition of the service in transactions/transactions.proto.
+
+--------
+
+# Run the sample code
+To compile and run the server, assuming you are in the root of the `stream-grpc`
+folder, simply:
+
+```sh
+$ go run server/main.go
+```
+
+Likewise, to run the client:
+
+```sh
+$ go run client/main.go
+```
+
---
+++ client/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "stream/transactions"
+)
+
+const (
+	address = "localhost:50051"
+	account = "121221-1"
+	branch  = "1212"
+)
+
+type data struct {
+	Account string
+	Branch  string
+}
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewTransactorClient(conn)
+
+	accountInformation := data{
+		Account: account,
+		Branch:  branch,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	stream, err := c.GetTransactions(ctx, &pb.TransactionsRequest{Account: accountInformation.Account, Branch: accountInformation.Branch})
+	if err != nil {
+		log.Fatalf("Could not get transactions: %v", err)
+	}
+	for {
+		data, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.GetTransactions(_) = _, %v", c, err)
+		}
+		log.Printf("ID: %s | date: %s | description: %s | amount: %s\n", data.Id, data.Transactions.Date, data.Transactions.Description, data.Transactions.Amount)
+
+	}
+}
